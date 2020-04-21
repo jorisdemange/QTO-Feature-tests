154ad88551a0f4089b8683b543f6d3b38525c8f2
Joris Demange <joris.demange@gmail.com>
2020-04-21 17:32:38 +0300

    commit

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-function openLeftMenu() {
-    document.getElementById("left-menu").classList.add("open")
-}
-function closeLeftMenu() {
-    document.getElementById("#left-menu").classList.remove("open");
+var leftMenu = document.getElementById("left-menu");
+var dimmer = document.getElementById("dimmer");
+var leftMenuBurger = document.getElementById("left-menu-burger");
+
+function openMenu() {
+	dimmer.classList.add("dimmer--open");
+	leftMenu.classList.add("left-menu--open");
 }
 
-window.addEventListener('mouseup', function(event) {
-    var leftMenu = document.getElementById('#left-menu');
-    if(event.target != leftMenu && event.target.parentNode != leftMenu) {
-        
-    }
+function closeMenu() {
+	dimmer.classList.remove("dimmer--open");
+	leftMenu.classList.remove("left-menu--open");
+}
+
+// When menu is closed
+leftMenuBurger.addEventListener("click", openMenu());
+
+// When menu is open
+document.addEventListener("click", (evt) => {
+	let targetElement = evt.target; // clicked element
+
+	do {
+		if (targetElement == leftMenu) {
+			//Detects a click in the menu
+			document.getElementById("menuDebug").textContent = "Clicked in the menu!";
+			return;
+		}
+		targetElement = targetElement.parentNode;
+	} while (targetElement);
+
+	//Detects a click outside of the menu
+	document.getElementById("menuDebug").textContent = "Clicked outside the menu!";
+	closeMenu();
 })
